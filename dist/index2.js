9ea20c053c1e18e42e9444aa8fdfed9ebc8c7dc6
Alexander <aleyva@recurly.com>
2022-04-27 01:10:40 -0300

    update bowser logs

--- dist/index2.js
+++ dist/index2.js
@@ -16,7 +16,7 @@
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var bowser_bundled__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! bowser/bundled */ \"./node_modules/bowser/bundled.js\");\n/* harmony import */ var bowser_bundled__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(bowser_bundled__WEBPACK_IMPORTED_MODULE_0__);\n\n\nconsole.log('Bowser', (bowser_bundled__WEBPACK_IMPORTED_MODULE_0___default()));\nconst browser = bowser_bundled__WEBPACK_IMPORTED_MODULE_0___default().getParser(window.navigator.userAgent);\nconsole.log('browser', browser);\n\n\n//# sourceURL=webpack:///./index2.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var bowser_bundled__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! bowser/bundled */ \"./node_modules/bowser/bundled.js\");\n/* harmony import */ var bowser_bundled__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(bowser_bundled__WEBPACK_IMPORTED_MODULE_0__);\n\n\nconsole.log('Bowser', (bowser_bundled__WEBPACK_IMPORTED_MODULE_0___default()));\n\nconst browser = bowser_bundled__WEBPACK_IMPORTED_MODULE_0___default().getParser(window.navigator.userAgent);\nconsole.log('browser', browser);\n\nconsole.log({mobile: browser.is('mobile'), tablet: browser.is('tablet'), ios: browser.is('ios') })\n\n\n//# sourceURL=webpack:///./index2.js?");
 
 /***/ }),
 
--- index2.js
+++ index2.js
@@ -1,5 +1,8 @@
 import Bowser from 'bowser/bundled';
 
 console.log('Bowser', Bowser);
+
 const browser = Bowser.getParser(window.navigator.userAgent);
 console.log('browser', browser);
+
+console.log({mobile: browser.is('mobile'), tablet: browser.is('tablet'), ios: browser.is('ios') })
