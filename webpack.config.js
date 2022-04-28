49dbd15b47008ccecf31d32daccbcd9b5d6dce4e
Alexander <aleyva@recurly.com>
2022-04-28 09:59:10 -0300

    wip

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 module.exports = {
   mode: 'development',
   entry: './index.js',
+  devtool: 'source-map',
   output: {
     filename: 'index.js',
     path: path.resolve(__dirname, 'dist'),
@@ -11,6 +12,7 @@ module.exports = {
   optimization: {
     minimizer: [
       new UglifyJsPlugin({
+        sourceMap: true,
         uglifyOptions: {
           output: { comments: false }
         }
